9555ba4f4aac3141f40405de1877fd2c875f971c
iyzu-code <iyzufizeyvin@gmail.com>
2025-11-13 13:07:47 +0700

    initial commit

---
+++ BasicProgramming/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BasicProgramming;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 17;
+        var status = "no status";
+        string name = "Ahmad";
+
+        age += 1;
+
+        if (age == 0)
+        {
+            name = "belum diberi nama";
+            status = "baru lahir";
+        }
+
+        switch (age)
+        {
+            case 0:
+                status = "baru lahir";
+                break;
+            case > 10:
+                break;
+            default:
+                break;
+        }
+
+        // Console.WriteLine(name);
+        // Console.WriteLine("Age: " + age);
+        // Console.WriteLine(status);
+
+        //looping
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        // int i = 1;
+        // while (i < 10)
+        // {
+        //     Console.WriteLine("loop" + i);
+        //     i++;
+        // }
+    }    
+}
